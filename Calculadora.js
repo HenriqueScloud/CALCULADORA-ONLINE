a98d649478a65c1023392527c6544d3a3e53c926
Henrique S. Mendes <124588969+HenriqueScloud@users.noreply.github.com>
2023-08-02 16:32:10 -0300

    NEW POST - CALCULADORA
    criando a calculadora com javascripit

--- Calculadora.js
+++ Calculadora.js
@@ -17,3 +17,51 @@ function btnSexo(){
 
 }
 btnSexo();
+
+
+// CRIANDO O CALCULO DO IMC COM OS DADOS FORNECIDOS
+
+
+function calcular(){
+    const alturaInput  = document.getElementById('altura')
+    const altura = alturaInput.value  // valor da altura da pessoa 
+
+    const pesoInput   = document.getElementById('peso')
+    const peso = pesoInput.value // valor do peso da pessoa 
+
+    let  resultadoInput = document.querySelector('.resultado-imc')
+
+
+
+
+    let IMC = peso / (altura*altura)
+     IMC = (IMC * 10000).toFixed(1)
+    resultadoInput.innerHTML=IMC
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
