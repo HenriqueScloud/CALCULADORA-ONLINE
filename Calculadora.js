23f3c8aac2459cda8541a34d08002190ac4afe90
Henrique S. Mendes <124588969+HenriqueScloud@users.noreply.github.com>
2023-08-02 17:34:04 -0300

    NEW POST - CALCULADORA
    ALERT

--- Calculadora.js
+++ Calculadora.js
@@ -1,4 +1,4 @@
-
+'use strict'
 
 
 // criando o btn do sexo para escolha do usuário
@@ -19,25 +19,98 @@ function btnSexo(){
 btnSexo();
 
 
-// CRIANDO O CALCULO DO IMC COM OS DADOS FORNECIDOS
+// // CRIANDO O CALCULO DO IMC COM OS DADOS FORNECIDOS
 
 
-function calcular(){
-    const alturaInput  = document.getElementById('altura')
-    const altura = alturaInput.value  // valor da altura da pessoa 
+// function calcular(){
+    
+//     const alturaInput  = document.getElementById('altura')
+//     const altura = alturaInput.value  // valor da altura da pessoa 
 
-    const pesoInput   = document.getElementById('peso')
-    const peso = pesoInput.value // valor do peso da pessoa 
+//     const pesoInput   = document.getElementById('peso')
+//     const peso = pesoInput.value // valor do peso da pessoa 
 
-    let  resultadoInput = document.querySelector('.resultado-imc')
+//     let  resultadoInput = document.querySelector('.resultado-imc')
+
+//     const inputs = document.querySelectorAll('.inputs')
+//     console.log(inputs)
 
 
 
 
-    let IMC = peso / (altura*altura)
-     IMC = (IMC * 10000).toFixed(1)
-    resultadoInput.innerHTML=IMC
+//     if ( peso == '' || altura =='' || idade ==''){
+//         inputs.forEach((item)=>{
+//             item.classList.add("error")
+//         })
+
+//         const button = document.querySelector('.header-IMC')
+
+//         const alert = document.createElement('p')
+//         alert.innerText="Preencha todos os campos ⚠️"
+//         alert.classList.add('alerta')
+
+//         button.appendChild(alert)
+        
+//     }
+//     else{
+//         let IMC = peso / (altura*altura)
+//         IMC = (IMC * 10000).toFixed(1)
+//         resultadoInput.innerHTML=IMC
+//         inputs.forEach((item)=>{
+//             item.classList.remove("error")
+//             button.appendChild(alert).remove
+//         })
+
+
+//     }
+
+
+    
+// }
+
+function calcular() {
+    const alturaInput = document.getElementById('altura');
+    const altura = alturaInput.value;
+
+    const pesoInput = document.getElementById('peso');
+    const peso = pesoInput.value;
+
+    let resultadoInput = document.querySelector('.resultado-imc');
+    const inputs = document.querySelectorAll('.inputs');
+
+
+
+
+
+    
+    const alerta = document.querySelector('.alerta');
+    if (alerta) {
+        alerta.remove();
+    }
+
+    if (peso == '' || altura == '' || idade == '') {
+    inputs.forEach((item) => {
+        item.classList.add('error');
+    });
+
+    const button = document.querySelector('.header-IMC');
+
+    const alert = document.createElement('p');
+    alert.innerText = 'Preencha todos os campos ⚠️';
+    alert.classList.add('alerta');
+
+    button.appendChild(alert);
+    } else {
+      let IMC = peso / (altura * altura);
+      IMC = (IMC * 10000).toFixed(1);
+    resultadoInput.innerHTML = IMC;
+    inputs.forEach((item) => {
+        item.classList.remove('error');
+    });
+    }
 }
+
+
 
 
 
